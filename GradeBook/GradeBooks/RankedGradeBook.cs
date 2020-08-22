daad3e43f14245ff9dc2634637eb045e8650283b
Jason Dailey <jdailey@hchb.com>
2020-08-22 16:37:48 -0400

    through module 2

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,12 @@
+﻿namespace GradeBook.GradeBooks
+{
+    using GradeBook.Enums;
+
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,13 @@
+﻿namespace GradeBook.GradeBooks
+{
+    using GradeBook.Enums;
+
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Standard;
+
+        }
+    }
+}
